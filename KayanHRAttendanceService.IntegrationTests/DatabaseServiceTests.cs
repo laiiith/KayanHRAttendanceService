086c8e35ff3f3b30c30b23b41fd752ce1d209800
mohannad ali <mohnandsroor181@gmail.com>
2025-05-29 11:26:24 +0300

    !!

---
+++ KayanHRAttendanceService.IntegrationTests/DatabaseServiceTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+using FluentAssertions;
+using System.Threading.Tasks;
+using KayanHRAttendanceService.WindowsService.Services;
+using KayanHRAttendanceService.WindowsService.Entities;
+using System;
+
+namespace KayanHRAttendanceService.IntegrationTests
+{
+    public class DatabaseServiceTests
+    {
+    }
+}
