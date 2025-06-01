df6a74d39f8869fe13c2b88f45dbd2e5bb1142b8
Laith Subbh <laith.m2002@outlook.com>
2025-06-01 10:26:43 +0300

    Delete Core Layer

--- Src/Core/KayanHRAttendanceService.Core/Interfaces/IAttendanceConnector.cs
+++
@@ -1,8 +0,0 @@
-﻿using KayanHRAttendanceService.Entities.Sqlite;
-
-namespace KayanHRAttendanceService.Core.Interfaces;
-
-public interface IAttendanceConnector
-{
-    Task<List<AttendanceRecord>> FetchAttendanceAsync();
-}
--- Src/Domain/KayanHRAttendanceService.Domain/Entities/General/APIRequest.cs
+++ Src/Domain/KayanHRAttendanceService.Domain/Entities/General/APIRequest.cs
@@ -3,12 +3,12 @@
 public class APIRequest
 {
     public HttpMethod Method { get; set; } = HttpMethod.Get;
-    public string Url { get; set; }
-    public object Data { get; set; }
+    public string? Url { get; set; }
+    public object? Data { get; set; }
     public HttpServiceContentTypes RequestContentType { get; set; } = HttpServiceContentTypes.application_json;
-    public string Token { get; set; }
-    public Dictionary<string, string> CustomHeaders { get; set; }
-    public Dictionary<string, string> QueryParameters { get; set; }
+    public string? Token { get; set; }
+    public Dictionary<string, string>? CustomHeaders { get; set; }
+    public Dictionary<string, string>? QueryParameters { get; set; }
 }
 public enum HttpServiceContentTypes
 {
