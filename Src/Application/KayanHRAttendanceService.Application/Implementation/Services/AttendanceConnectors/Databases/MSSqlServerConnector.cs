a3dddbfb9c1024bcfeeff6428653b85d489ebcd1
mohannad ali <mohnandsroor181@gmail.com>
2025-06-11 16:57:42 +0300

    SQL SERVER FIX

--- Src/Application/KayanHRAttendanceService.Application/Implementation/Services/AttendanceConnectors/Databases/MSSqlServerConnector.cs
+++ Src/Application/KayanHRAttendanceService.Application/Implementation/Services/AttendanceConnectors/Databases/MSSqlServerConnector.cs
@@ -41,5 +41,5 @@ public class MSSqlServerConnector(IOptions<IntegrationSettings> settingsOptions,
 
     protected override string GetCreateTempTableSql() => "CREATE TABLE #TempTVP(tid INT,flag INT DEFAULT 1);";
 
-    protected override string GetInsertTempTableSql() => "INSERT INTO #TempTVP (tid,flag) VALUES (@tid,@flag)";
+    protected override string GetInsertTempTableSql() => "INSERT INTO #Temp (TId,Flag) VALUES (@tid,@flag)";
 }
